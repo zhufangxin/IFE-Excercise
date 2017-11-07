7976c9acf55fd0320c2c82f5f4087bd4020ba288
Fangxin Zhu <878237422@qq.com>
2017-11-07 16:07:38 +0800

    Add files via upload

--- task30/task30.js
+++ task30/task30.js
@@ -7,4 +7,38 @@
 //点击提交按钮时，对页面中所有输入进行校验，校验结果显示方式同上。若所有表单校验通过，弹
 //窗显示“提交成功”，否则显示“提交失败”
 
+var username=document.getElementById("username");
+var status=document.getElementById("status");
+var inputs=document.getElementsByTagName("input");
 
+var validations={
+	namevalidate: function(){
+		if(username.value.length==0){
+			this.wrongValidate(username,"名称不能为空");
+		}
+		else if(username.value.length<4 || username.value.length>16){			
+			this.wrongValidate(username,"请输入4~16个字符");
+		}else{
+			this.rightValidate(username,"名称可用");
+		}
+		
+	},
+	wrongValidate: function(ele,comment){
+		ele.className="ierror";
+		ele.nextElementSibling.className="perror";
+		ele.nextElementSibling.innerHTML=comment;
+	},
+	rightValidate: function(){
+		ele.className="iright";
+		ele.nextElementSibling.className="pright";
+		ele.nextElementSibling.innerHTML=comment;
+	}
+}
+for(var i=0;i<inputs.length;i++){
+	inputs[i].addEventListener("focus", function(){
+		this.nextElementSibling.className="show";
+	})
+	inputs[i].addEventListener("blur", function(){
+		validations.namevalidate();
+	})
+}
