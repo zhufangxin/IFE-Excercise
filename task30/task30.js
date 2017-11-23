fb2721df1ddb2e67f3cc75f5036a7c4042360a8d
Fangxin Zhu <878237422@qq.com>
2017-11-23 15:52:07 +0800

    Add files via upload

--- task30/task30.js
+++ task30/task30.js
@@ -8,11 +8,15 @@
 //窗显示“提交成功”，否则显示“提交失败”
 
 var username=document.getElementById("username");
+var password=document.getElementById("password");
+var repassword=document.getElementById("repassword");
+var email=document.getElementById("email");
 var status=document.getElementById("status");
 var inputs=document.getElementsByTagName("input");
+var submit=document.getElementById("submit");
 
 var validations={
-	namevalidate: function(){
+	nameValidate: function(){
 		if(username.value.length==0){
 			this.wrongValidate(username,"名称不能为空");
 		}
@@ -22,6 +26,34 @@ var validations={
 			this.rightValidate(username,"名称可用");
 		}
 		
+	},
+	passwordValidate: function(){
+		if(password.value.length==0) {
+			this.wrongValidate(password,"密码不能为空");
+		}else{
+			this.rightValidate(password,"密码可用");
+		}
+	},
+	repasswordValidate: function(){
+		if(repassword.value.length==0) {
+			this.wrongValidate(repassword,"确认密码不能为空");
+		}
+		else if(password.value!== repassword.value){
+			this.wrongValidate(repassword,"两次密码不一致");
+		}else{
+			this.rightValidate(repassword,"密码可用");
+		}
+	},
+	emailValidate: function() {
+		var regExp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+		if(email.value.length==0){
+			this.wrongValidate(email,"邮箱不能为空");
+		}
+		else if(!email.value.match(regExp)){
+			this.wrongValidate(email,"邮箱格式不正确");
+		}else{
+			this.rightValidate(email,"邮箱可用");
+		}
 	},
 	wrongValidate: function(ele,comment){
 		ele.className="ierror";
@@ -39,6 +71,37 @@ for(var i=0;i<inputs.length;i++){
 		this.nextElementSibling.className="show";
 	})
 	inputs[i].addEventListener("blur", function(){
-		validations.namevalidate();
+		switch(this.id){
+			case 'username':
+				validations.nameValidate();
+				break;
+			case 'password':
+				validations.passwordValidate();
+				break;	
+			case 'repassword':
+				validations.repasswordValidate();
+				break;
+			case 'email':
+				validations.emailValidate();
+				break;
+		}		
 	})
+}
+
+submit.onclick = function (){
+	var flag=0;
+	for(var opt in inputs){
+		console.log(opt);
+		if(opt.className="ierror"){
+			flag ++;
+			
+		}
+		
+	}
+	if(flag>0){
+		alert("wrong");
+	}
+	else {
+		alert("pass");
+		}
 }
