f3feed108c66c1ca7bdb1e6ed9d1f733bb9ffe3a
Fangxin Zhu <878237422@qq.com>
2017-12-01 15:57:44 +0800

    Add files via upload

--- task31/task31.js
+++ task31/task31.js
@@ -2,4 +2,9 @@
 //当选择在校生时，出现两个select下拉菜单，一个选择城市，一个选择学校，当选择非在校生时，出一个文本输入框
 //学校下拉菜单里的学校名单均为城市下拉菜单中所选的城市中的大学，当城市发生变化时，学校一起发生变化
 //城市及学校的数据随意编造即可，无需真实完整
-
+var radios= document.getElementsByName("stutype");
+for(var i=0;i<radios.length;i++){
+		radios[i].onclick = function(){
+		alert(this.id);
+	} 
+}
